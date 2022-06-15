6fdbb41232d2749049c8fe4c63a56d7cfd20a858
chiyu <chiyuchao0210@gmail.com>
2022-06-16 02:43:10 +0800

    Updates

---
+++ precache-manifest.c9b209cb1bd4af857b8f402fe1ec27d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "db29f0adbdbbb365de5d",
+    "url": "/math-game/static/css/main.fea596ce.chunk.css"
+  },
+  {
+    "revision": "db29f0adbdbbb365de5d",
+    "url": "/math-game/static/js/main.db29f0ad.chunk.js"
+  },
+  {
+    "revision": "5fc6eca808e7c1951395",
+    "url": "/math-game/static/js/1.5fc6eca8.chunk.js"
+  },
+  {
+    "revision": "c139726765860a190f7a",
+    "url": "/math-game/static/js/2.c1397267.chunk.js"
+  },
+  {
+    "revision": "2f384badc311ff000cd6",
+    "url": "/math-game/static/js/runtime~main.2f384bad.js"
+  },
+  {
+    "revision": "1472b5cf280bf1c291c544befc563c10",
+    "url": "/math-game/static/media/failureSound.1472b5cf.wav"
+  },
+  {
+    "revision": "38029e5a3f7d71b40b6e367a14816117",
+    "url": "/math-game/static/media/correctSound.38029e5a.wav"
+  },
+  {
+    "revision": "6840c57684c44e190c4eefc33f6848d0",
+    "url": "/math-game/index.html"
+  }
+];
