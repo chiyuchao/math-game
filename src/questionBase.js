22f68892b8d51f432f5ecd6e8c0274b504e6a718
chiyu <chiyuchao0210@gmail.com>
2022-06-23 13:33:08 +0800

    main

--- src/questionBase.js
+++ src/questionBase.js
@@ -1,6 +1,6 @@
 export const data = [
-  { id: "1", difficulty: "level 1", question: [2, "A", "+", "B"] },
-  { id: "2", difficulty: "level 2", question: ["A", "-", 4, "B"] },
+  { id: "1", difficulty: "level 1", question: ["A", "-", 4, "B"] },
+  { id: "2", difficulty: "level 2", question: [2, "A", "+", "B"] },
   { id: "3", difficulty: "level 3", question: [3, "A", "-", 2, "B"] },
   { id: "4", difficulty: "level 4", question: ["A", "+", 3, "B", "-", 1] },
   { id: "5", difficulty: "level 5", question: [3, "A", "+", 2, "B", "-", 7] },
